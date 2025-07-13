a2c044b7f16e51f60e8b106b9f68bd20f0d4b047
Christian González Di Antonio <christiangda@gmail.com>
2025-07-13 17:19:36 +0200

    fix: set user as admin

--- database/migrations/1100_users_tables_insert.sql
+++ database/migrations/1100_users_tables_insert.sql
@@ -2,14 +2,15 @@
 -- +goose StatementBegin
 
 -- table users
-INSERT INTO users (id, first_name, last_name, email, password_hash, disabled) VALUES
+INSERT INTO users (id, first_name, last_name, email, password_hash, disabled, admin) VALUES
 -- user Administrator
 ('019791d2-adef-76d2-a865-5b19e5073e60',
  'Administrator',
  'Default',
  'admin@qu3ry.me',
  '$2a$10$GBQxEIIhh3MBQdFoHZ.wrej4l4ak26X1c5uvrLJmjrjSZ.VWXnX9G', -- password is 'ThisIsApassw0rd.,' hashed with bcrypt and salt
- FALSE);
+ FALSE,
+ TRUE);
 
 -- +goose StatementEnd
 
